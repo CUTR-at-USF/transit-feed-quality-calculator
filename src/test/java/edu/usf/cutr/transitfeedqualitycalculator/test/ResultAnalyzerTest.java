f369d31699023d80e41f89f35e26b719b748dca2
Sean Barbeau <sjbarbeau@gmail.com>
2018-01-26 16:08:51 -0500

    Fix #20 - Add unit tests for ResultsAnalyzer (#26)
    Adds test cases to test all the graph data in the Excel sheet

---
+++ src/test/java/edu/usf/cutr/transitfeedqualitycalculator/test/ResultAnalyzerTest.java
@@ -0,0 +1,383 @@
+package edu.usf.cutr.transitfeedqualitycalculator.test;
+
+import edu.usf.cutr.gtfsrtvalidator.lib.validation.ValidationRules;
+import edu.usf.cutr.transitfeedqualitycalculator.ResultsAnalyzer;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.assertEquals;
+
+public class ResultAnalyzerTest {
+
+    @org.junit.jupiter.api.Test
+    public void resultAnalyzerTest() throws java.io.IOException, java.lang.NoSuchFieldException, java.lang.IllegalAccessException {
+        String directoryName = "src/test/resources/feeds";
+        String workbookName = "graphs.xlsx";
+        ResultsAnalyzer analyzer = new ResultsAnalyzer(Paths.get(directoryName), "", "");
+        analyzer.analyzeResults();
+        Workbook workbook = new XSSFWorkbook(new FileInputStream(workbookName));
+        testDataSheet(workbook);
+        testSummarySheet(workbook);
+        testFrequencySheet(workbook);
+        testCountSheet(workbook);
+        testHistogramSheet(workbook);
+    }
+
+    private void testHistogramSheet(Workbook workbook) {
+        Sheet summarySheet = workbook.getSheet("Histogram");
+        Row row = summarySheet.getRow(1);
+        Cell cell = row.getCell(0);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(2);
+        cell = row.getCell(0);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(3);
+        cell = row.getCell(0);
+        assertEquals(3, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(4);
+        cell = row.getCell(0);
+        assertEquals(4, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(5);
+        cell = row.getCell(0);
+        assertEquals(5, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(6);
+        cell = row.getCell(0);
+        assertEquals(6, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(7);
+        cell = row.getCell(0);
+        assertEquals(7, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(8);
+        cell = row.getCell(0);
+        assertEquals(8, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(10);
+        cell = row.getCell(0);
+        assertEquals("Total", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(5, cell.getNumericCellValue(), 0);
+
+
+        row = summarySheet.getRow(13);
+        cell = row.getCell(0);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(14);
+        cell = row.getCell(0);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(3, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(15);
+        cell = row.getCell(0);
+        assertEquals(3, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(16);
+        cell = row.getCell(0);
+        assertEquals(4, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(17);
+        cell = row.getCell(0);
+        assertEquals(5, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(18);
+        cell = row.getCell(0);
+        assertEquals(6, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(19);
+        cell = row.getCell(0);
+        assertEquals(7, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(20);
+        cell = row.getCell(0);
+        assertEquals(8, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(0, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(22);
+        cell = row.getCell(0);
+        assertEquals("Total", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(6, cell.getNumericCellValue(), 0);
+    }
+
+    private void testCountSheet(Workbook workbook) {
+        Sheet summarySheet = workbook.getSheet("Error Count");
+        Row row = summarySheet.getRow(1);
+        Cell cell = row.getCell(0);
+        assertEquals("MTA Maryland Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(6, cell.getNumericCellValue(), 0);
+        cell = row.getCell(3);
+        assertEquals("HART Vehicle Positions", cell.getStringCellValue());
+        cell = row.getCell(4);
+        assertEquals(3, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(2);
+        cell = row.getCell(0);
+        assertEquals("MTA Maryland Vehicle Locations", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(4, cell.getNumericCellValue(), 0);
+        cell = row.getCell(3);
+        assertEquals("HART Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(4);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(3);
+        cell = row.getCell(0);
+        assertEquals("MetroTransit Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+        cell = row.getCell(3);
+        assertEquals("MTA Maryland Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(4);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(4);
+        cell = row.getCell(0);
+        assertEquals("MetroTransit Vehicle Positions", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+        cell = row.getCell(3);
+        assertEquals("MetroTransit Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(4);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(5);
+        cell = row.getCell(0);
+        assertEquals("HART Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+        cell = row.getCell(3);
+        assertEquals("MTA Maryland Vehicle Locations", cell.getStringCellValue());
+        cell = row.getCell(4);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(6);
+        cell = row.getCell(3);
+        assertEquals("MetroTransit Vehicle Positions", cell.getStringCellValue());
+        cell = row.getCell(4);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+    }
+
+    private void testFrequencySheet(Workbook workbook) {
+        Sheet summarySheet = workbook.getSheet("Error Frequency");
+        Row row = summarySheet.getRow(1);
+        Cell cell = row.getCell(0);
+        assertEquals("E017 - " + ValidationRules.E017.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(4, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(2);
+        cell = row.getCell(0);
+        assertEquals("E003 - " + ValidationRules.E003.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(3);
+        cell = row.getCell(0);
+        assertEquals("E022 - " + ValidationRules.E022.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(4);
+        cell = row.getCell(0);
+        assertEquals("E011 - " + ValidationRules.E011.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(5);
+        cell = row.getCell(0);
+        assertEquals("E028 - " + ValidationRules.E028.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(6);
+        cell = row.getCell(0);
+        assertEquals("E029 - " + ValidationRules.E029.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(7);
+        cell = row.getCell(0);
+        assertEquals("E041 - " + ValidationRules.E041.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(8);
+        cell = row.getCell(0);
+        assertEquals("W009 - " + ValidationRules.W009.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(6, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(9);
+        cell = row.getCell(0);
+        assertEquals("W001 - " + ValidationRules.W001.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(4, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(10);
+        cell = row.getCell(0);
+        assertEquals("W004 - " + ValidationRules.W004.getTitle(), cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+    }
+
+    private void testSummarySheet(Workbook workbook) {
+        Sheet summarySheet = workbook.getSheet("Summary");
+        Row row = summarySheet.getRow(0);
+        Cell cell = row.getCell(0);
+        assertEquals("Feeds with errors", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Feeds without errors", cell.getStringCellValue());
+
+        row = summarySheet.getRow(1);
+        cell = row.getCell(0);
+        assertEquals(5, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(2, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(3);
+        cell = row.getCell(0);
+        assertEquals("Feeds with warnings", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Feeds without warnings", cell.getStringCellValue());
+
+        row = summarySheet.getRow(4);
+        cell = row.getCell(0);
+        assertEquals(6, cell.getNumericCellValue(), 0);
+        cell = row.getCell(1);
+        assertEquals(1, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(12);
+        cell = row.getCell(0);
+        assertEquals("Total feeds processed", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(7, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(13);
+        cell = row.getCell(0);
+        assertEquals("Feeds with errors", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(5, cell.getNumericCellValue(), 0);
+
+        row = summarySheet.getRow(14);
+        cell = row.getCell(0);
+        assertEquals("Feeds with warnings", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals(6, cell.getNumericCellValue() , 0);
+    }
+
+    private void testDataSheet(Workbook workbook) {
+        Sheet dataSheet = workbook.getSheet("Data");
+        Row row = dataSheet.getRow(1);
+        Cell cell = row.getCell(0);
+        assertEquals("MTA Maryland Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Maryland, USA", cell.getStringCellValue());
+        cell = row.getCell(2);
+        assertEquals("E022, E037, E041, E003, E004, E011", cell.getStringCellValue());
+        cell = row.getCell(3);
+        assertEquals("W001, W009", cell.getStringCellValue());
+
+        row = dataSheet.getRow(2);
+        cell = row.getCell(0);
+        assertEquals("MTA Maryland Vehicle Locations", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Maryland, USA", cell.getStringCellValue());
+        cell = row.getCell(2);
+        assertEquals("E028, E017, E003, E004", cell.getStringCellValue());
+        cell = row.getCell(3);
+        assertEquals("W009", cell.getStringCellValue());
+
+        row = dataSheet.getRow(3);
+        cell = row.getCell(0);
+        assertEquals("HART Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Tampa, FL, USA", cell.getStringCellValue() );
+        cell = row.getCell(2);
+        assertEquals("E017", cell.getStringCellValue());
+        cell = row.getCell(3);
+        assertEquals("W001, W009", cell.getStringCellValue());
+
+        row = dataSheet.getRow(4);
+        cell = row.getCell(0);
+        assertEquals("HART Vehicle Positions", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Tampa, FL, USA", cell.getStringCellValue());
+        cell = row.getCell(2);
+        assertEquals("", cell.getStringCellValue());
+        cell = row.getCell(3);
+        assertEquals("W004, W001, W009", cell.getStringCellValue());
+
+        row = dataSheet.getRow(5);
+        cell = row.getCell(0);
+        assertEquals("MetroTransit Service Alerts", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Halifax, NS, Canada", cell.getStringCellValue());
+        cell = row.getCell(2);
+        assertEquals("", cell.getStringCellValue());
+        cell = row.getCell(3);
+        assertEquals("", cell.getStringCellValue());
+
+        row = dataSheet.getRow(6);
+        cell = row.getCell(0);
+        assertEquals("MetroTransit Trip Updates", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Halifax, NS, Canada", cell.getStringCellValue());
+        cell = row.getCell(2);
+        assertEquals("E017, E022", cell.getStringCellValue());
+        cell = row.getCell(3);
+        assertEquals("W001, W009", cell.getStringCellValue());
+
+        row = dataSheet.getRow(7);
+        cell = row.getCell(0);
+        assertEquals("MetroTransit Vehicle Positions", cell.getStringCellValue());
+        cell = row.getCell(1);
+        assertEquals("Halifax, NS, Canada", cell.getStringCellValue());
+        cell = row.getCell(2);
+        assertEquals("E029, E017", cell.getStringCellValue());
+        cell = row.getCell(3);
+        assertEquals("W009", cell.getStringCellValue());
+    }
+}
